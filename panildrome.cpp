d86ab00b61df558a1f6e583c060f3f2f8252cc19
bhuwneshwa <97431962+bhuwneshwa@users.noreply.github.com>
2022-04-11 09:27:55 +0530

    Create panildrome.cpp

---
+++ panildrome.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+        char str[30];
+        int i, length;
+        int flag = 0;    
+
+        cout<<"Enter String : "<<;
+        cin>>str;    
+        length = strlen(str);    
+        for(i=0;i < length ;i++)
+        {
+                if(str[i] != str[length-i-1])
+                {
+                        flag = 1;
+                        break;
+                }
+        }    
+        if(flag)
+        {
+                cout<<" "<<str<<" is not a palindrome"<<endl;
+        }    
+        else
+        {
+                cout<<" "<<str<< " is a palindrome"<<endl;
+        }
+        return 0;
+}
