4bcfd84c6cce8974c263f2a957efdae5f7209041
bhuwneshwa <97431962+bhuwneshwa@users.noreply.github.com>
2022-04-11 08:57:14 +0530

    Create Pattern21.java

---
+++ Pattern21.java
@@ -0,0 +1,27 @@
+package Pattern;
+
+public class Pattern21 {
+    public static void main(String args[])
+    {
+        printPatten();
+    }
+
+    private static void printPatten() {
+        int i,j;
+        for(i=1;i<=4;i++)
+        {
+            for(j=1;j<=8;j++)
+            {
+                if(j>=5-i && j<=9-i)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
