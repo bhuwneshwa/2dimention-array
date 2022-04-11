a8752569b5fc3b28bf5281697e5990b82df50d8c
bhuwneshwa <97431962+bhuwneshwa@users.noreply.github.com>
2022-04-11 09:13:03 +0530

    Create Pattern9.java

---
+++ Pattern9.java
@@ -0,0 +1,35 @@
+package Pattern;
+
+public class Pattern9 {
+    public static void main(String args[])
+    {
+        printPatten();
+    }
+
+    private static void printPatten() {
+        int i,j;
+        char k;
+        for(i=1;i<=4;i++)
+        {
+            k = 'A';
+            for(j=1;j<=7;j++)
+            {
+                if(j<=5-i || j>=3+i)
+                {
+                    System.out.print(k);
+                    if(j<4)
+                        k++;
+                    else
+                        k--;
+                }
+                else
+                {
+                    System.out.print(" ");
+                    if(j==4)
+                        k--;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
